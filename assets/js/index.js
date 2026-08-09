2b6f9589dee6002d1583dc23e3bdc75d5d1fb82d
Viktoria Melnyk <melnukvikysa@gmail.com>
2026-08-09 23:59:53 +0300

    Add JSDoc for function check multiples of numbers

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,14 +9,19 @@
 // console.log(isWorkingAgePerson(64));
 // console.log(isWorkingAgePerson(70));
 
+/**
+ *  Check if the first number divides evenly by the second
+ * @param {number} num1 - First number
+ * @param {number} num2 - Second number
+ * @returns {boolean} - true or false
+ */
 const checkMultiplicity = function (num1, num2) {
   return num1 % num2 === 0;
 };
 
-console.log(checkMultiplicity(10,2));
-console.log(checkMultiplicity(11,2));
-console.log(checkMultiplicity(10,5));
-console.log(checkMultiplicity(10,3));
-console.log(checkMultiplicity(15,3));
-console.log(checkMultiplicity(1,2));
-
+console.log(checkMultiplicity(10, 2));
+console.log(checkMultiplicity(11, 2));
+console.log(checkMultiplicity(10, 5));
+console.log(checkMultiplicity(10, 3));
+console.log(checkMultiplicity(15, 3));
+console.log(checkMultiplicity(1, 2));
