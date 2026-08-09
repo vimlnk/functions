76c299277c196db482e19ab8f0d5bd39e75a7a80
Viktoria Melnyk <melnukvikysa@gmail.com>
2026-08-09 23:10:17 +0300

    Calculation the number of paint cans

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,22 +20,30 @@
 // console.log(checkMultiplicity(15,3));
 // console.log(checkMultiplicity(1,2));
 
-function calculateSurfaceArea(shapeType, param1, param2 = 1) {
-  if (shapeType === "cube" && param1 > 0 && param2 > 0) {
-    return 6 * param1 ** 2;
-  }
-  if (shapeType === "cylinder" && param1 > 0 && param2 > 0) {
-    return 2 * Math.PI * param1 * (param1 + param2);
-  } else {
-    return 0;
-  }
+// function calculateSurfaceArea(shapeType, param1, param2 = 1) {
+//   if (shapeType === "cube" && param1 > 0 && param2 > 0) {
+//     return 6 * param1 ** 2;
+//   }
+//   if (shapeType === "cylinder" && param1 > 0 && param2 > 0) {
+//     return 2 * Math.PI * param1 * (param1 + param2);
+//   } else {
+//     return 0;
+//   }
+// }
+
+// console.log(calculateSurfaceArea("cube", 3));
+// console.log(calculateSurfaceArea("cube", 3, 3));
+// console.log(calculateSurfaceArea("cbe", 3, 3));
+// console.log(calculateSurfaceArea("cube", -3, 3));
+// console.log(calculateSurfaceArea("cube", 3, 0));
+
+const calculatePaintBoxes = function (area, layersCount =1){
+return Math.ceil(area*0.1*layersCount);
 }
 
-console.log(calculateSurfaceArea("cube", 3));
-console.log(calculateSurfaceArea("cube", 3, 3));
-console.log(calculateSurfaceArea("cbe", 3, 3));
-console.log(calculateSurfaceArea("cube", -3, 3));
-console.log(calculateSurfaceArea("cube", 3, 0));
+console.log(calculatePaintBoxes(20,1));
+console.log(calculatePaintBoxes(25,3));
+console.log(calculatePaintBoxes(35,5));
 
 
 
