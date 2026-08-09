c2a23107460916bdafa3ffc108d2127fed5522cc
Viktoria Melnyk <melnukvikysa@gmail.com>
2026-08-09 21:43:31 +0300

    Add checking working age

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
+function isWorkingAgePerson(age) {
+  return age >= 16 && age <= 64;
+}
 
-
-
-
+console.log(isWorkingAgePerson(16));
+console.log(isWorkingAgePerson(13));
+console.log(isWorkingAgePerson(18));
+console.log(isWorkingAgePerson(30));
+console.log(isWorkingAgePerson(64));
+console.log(isWorkingAgePerson(70));
