49be59d2bae2a1fdc6cb5380eba12c751ff4e8a4
Viktoria Melnyk <melnukvikysa@gmail.com>
2026-08-09 21:54:47 +0300

    Add checking the multiples of numbers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
-function isWorkingAgePerson(age) {
-  return age >= 16 && age <= 64;
-}
+// function isWorkingAgePerson(age) {
+//   return age >= 16 && age <= 64;
+// }
 
-console.log(isWorkingAgePerson(16));
-console.log(isWorkingAgePerson(13));
-console.log(isWorkingAgePerson(18));
-console.log(isWorkingAgePerson(30));
-console.log(isWorkingAgePerson(64));
-console.log(isWorkingAgePerson(70));
+// console.log(isWorkingAgePerson(16));
+// console.log(isWorkingAgePerson(13));
+// console.log(isWorkingAgePerson(18));
+// console.log(isWorkingAgePerson(30));
+// console.log(isWorkingAgePerson(64));
+// console.log(isWorkingAgePerson(70));
+
+const checkMultiplicity = function (num1, num2) {
+  return num1 % num2 === 0;
+};
+
+console.log(checkMultiplicity(10,2));
+console.log(checkMultiplicity(11,2));
+console.log(checkMultiplicity(10,5));
+console.log(checkMultiplicity(10,3));
+console.log(checkMultiplicity(15,3));
+console.log(checkMultiplicity(1,2));
+
